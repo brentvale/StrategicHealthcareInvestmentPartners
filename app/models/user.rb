b4a594bb2103da73b6cbac9d16015c6bb87b8bca
Brent Vale <brentvale@gmail.com>
2016-09-28 10:05:36 -0700

    adds devise confirmable (just to be safe)

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :lockable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
   
   validate :limit_emails     
          
